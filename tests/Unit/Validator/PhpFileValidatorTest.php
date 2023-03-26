e5be48c5207b308f207a06120d9ae33bb76140ec
Olexandr Mazur <alexandrmazur96@gmail.com>
2023-03-26 18:47:31 +0300

    Added tests for PhpFileValidator

---
+++ tests/Unit/Validator/PhpFileValidatorTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCodeMinifier\Tests\Unit\Validator;
+
+use PhpCodeMinifier\Exceptions\IncorrectFileException;
+use PhpCodeMinifier\Tests\TestCase;
+use PhpCodeMinifier\Validator\PhpFileValidator;
+
+final class PhpFileValidatorTest extends TestCase
+{
+    private PhpFileValidator $validator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->validator = new PhpFileValidator();
+    }
+
+    protected function tearDown(): void
+    {
+        unset($this->validator);
+        parent::tearDown();
+    }
+
+    /** @throws IncorrectFileException */
+    public function testValidateFileHasPhpExtension(): void
+    {
+        $this->expectNotToPerformAssertions();
+        $this->validator->validate(__DIR__ . '/../../Fixtures/ActualFiles/PhpClass.php');
+    }
+
+    public function testValidatorFileIsDirectory(): void
+    {
+        $filePath = __DIR__ . '/../../Fixtures/ActualFiles';
+        $this->expectException(IncorrectFileException::class);
+        $this->expectExceptionMessage('File (' . $filePath . ') is a directory.');
+        $this->validator->validate($filePath);
+    }
+
+    public function testValidatorFileNotExists(): void
+    {
+        $filePath = __DIR__ . '/../../Fixtures/ActualFiles/file_not_exists.php';
+        $this->expectException(IncorrectFileException::class);
+        $this->expectExceptionMessage('File (' . $filePath . ') not found.');
+        $this->validator->validate($filePath);
+    }
+
+    public function testValidateFileHasNotPhpExtension(): void
+    {
+        $filePath = __DIR__ . '/../../Fixtures/ActualFiles/non-php-file.txt';
+        $this->expectException(IncorrectFileException::class);
+        $this->expectExceptionMessage('File (' . $filePath . ') must have .php extension.');
+        $this->validator->validate($filePath);
+    }
+}
